4a2f0472ee2bb2dd79a7b310aa9686421cad7ff1
AFS0011960 <AFS0011960@agderskolen.no>
2024-10-05 12:53:14 +0200

    Finished part A

--- part_A.py
+++ part_A.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 """
 TECH2 mandatory assignment - Part A
 
@@ -17,6 +19,20 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    n = 0
+    sum = 0
+    sumsquared = 0
+
+    #Calculating sum of array
+    for i in x:
+        n += 1
+        sum += i
+        sumsquared += i*i
+    #computing variance
+    var =((1/n)*sumsquared)-((1/n)*sum)**2
+    #computing standard deviation
+    sd = math.sqrt(var)
+    return sd
 
 def std_builtin(x):
     """
@@ -32,4 +48,17 @@ def std_builtin(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    n = len(x)
+    n_sum = sum(x)
+    sumsquared = sum(i*i for i in x)
+    #compute variance
+    var =((1/n)*sumsquared)-((1/n)*n_sum)**2
+    #computing standard deviation
+    sd = math.sqrt(var)
+    return sd
+
+num_lst = [1, 2, 3, 4, 5]
+print(std_loops(num_lst))
+print(std_builtin(num_lst))
+print(np.std(num_lst))
     
